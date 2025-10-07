5d633d289cbcc6851701cb537867728638843899
octubre1100 <136345512+vikyflores26@users.noreply.github.com>
2025-10-07 11:10:01 -0300

    .

--- apiss.js
+++ apiss.js
@@ -8,4 +8,15 @@
  return n*1 n2
  }*/
 
-fetch ("https://openf1.org/")
+fetch ("https://openf1.org/9159")
+.then(res => res. json())
+•then (productos => {
+//Escribo Lo que quiero que muestre la pagina
+Let articulo = document. querySelector(.productos)
+
+for ( let index = o; index < 3; index++){
+     articulo. innertHTML += <div> 
+    <h2>${producto[index].title}</h2
+    <img src="${producto[index].image}">
+    <p>{producto[index].decription}
+}
