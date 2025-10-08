1b1a3c6963bbdac1aead78e8f8af1c4589c4a162
alumno <alumno@Instituto.local>
2025-10-08 08:24:34 -0300

    .

--- apiss.js
+++ apiss.js
@@ -7,16 +7,20 @@
  function calculo (n1,n2){
  return n*1 n2
  }*/
+  let key = "https://api.openf1.org/v1/car_data?driver_number=55&session_key=9159&speed%3E=315"
+ 
 
-fetch ("https://openf1.org/9159")
-.then(res => res. json())
-•then (productos => {
-//Escribo Lo que quiero que muestre la pagina
-Let articulo = document. querySelector(.productos)
 
-for ( let index = o; index < 3; index++){
-     articulo. innertHTML += <div> 
-    <h2>${producto[index].title}</h2
-    <img src="${producto[index].image}">
-    <p>{producto[index].decription}
+ boton.onclick = function () {
+    fetch('https://d3t32hsnjxo7q6.cloudfront.net/i/991799d3e70b8856686979f8ff6dcfe0_ra,w158,h184_pa,w158,h184.png')
+    .then(res => res.json())    
+    .then(productos => {
+            let articulo = document.querySelector('.productos')
+
+            for (let index = 0; index < 3; index++) {
+                articulo.innerHTML = `<h2>${producto.title}</h2>
+<img src= "${producto.image}">
+<p>${producto.description}</p>`
+            }
+        })
 }
